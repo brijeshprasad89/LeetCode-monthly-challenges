88d678ad78dfdcb205800927c1987ee0eba081f4
Brijesh Prasad <brijesh.prasad@omio.com>
2021-12-10 14:47:33 +0100

    sum of unique elements

---
+++ src/com/leetcode/easy/SumOfUniqueElements.java
@@ -0,0 +1,12 @@
+package com.leetcode.easy;
+
+public class SumOfUniqueElements {
+    public static void main(String[] args) {
+        sumOfUnique(new int[]{1, 3, 3, 4});
+    }
+
+    static void sumOfUnique(int[] nums) {
+
+
+    }
+}
